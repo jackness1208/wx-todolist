7f845cb88e0605d8f34d796ba83874b662b30ab3
jackness <jackness1208@qq.com>
2019-06-16 01:01:40 +0800

    todo-frame 开发中

---
+++ components/todo-frame/todo-frame.js
@@ -0,0 +1,35 @@
+Component({
+  data: {
+    todoGroup: [{
+      title: 'todo group 01',
+      items: [{
+        checked: false,
+        content: 'todo item 01'
+      }, {
+        checked: false,
+        content: 'todo item 02'
+      }, {
+        checked: false,
+        content: 'todo item 03'
+      }, {
+        checked: false,
+        content: 'todo item 04'
+      }]
+    }, {
+      title: 'todo group 02',
+      items: [{
+        checked: true,
+        content: 'todo item 01'
+      }, {
+        checked: false,
+        content: 'todo item 02'
+      }, {
+        checked: false,
+        content: 'todo item 03'
+      }, {
+        checked: false,
+        content: 'todo item 04'
+      }]
+    }]
+  }
+})
