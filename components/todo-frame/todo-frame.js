00f4a785cefa5b8c13dd94bbab5da56cd46edce0
jackness <jackness1208@qq.com>
2019-06-16 20:22:34 +0800

    表单交互部分完成

--- components/todo-frame/todo-frame.js
+++ components/todo-frame/todo-frame.js
@@ -1,5 +1,6 @@
 Component({
   data: {
+    addItemValue: [],
     todoGroup: [{
       title: 'todo group 01',
       items: [{
@@ -31,5 +32,45 @@ Component({
         content: 'todo item 04'
       }]
     }]
+  },
+  methods: {
+    addItem(e) {
+      const val = e.detail.value
+      const { index } = e.target.dataset
+      const { todoGroup, addItemValue } = this.data
+      todoGroup[index].items.push({
+        checked: false,
+        content: val.trim()
+      })
+      addItemValue[index] = ''
+      this.setData({ todoGroup, addItemValue })
+    },
+    editItem(e) {
+      const { index, subindex } = e.target.dataset
+      const { value } = e.target
+      const { todoGroup } = this.data
+      const item = todoGroup[index].items[subindex]
+      if (value.trim() === '') {
+        todoGroup[index].items.splice(subindex, 1)
+      } else {
+        item.content = value
+      }
+      this.setData({ todoGroup })
+    },
+    checkboxChange(e) {
+      const { index, subindex } = e.target.dataset
+      const { todoGroup } = this.data
+      const item = todoGroup[index].items[subindex]
+      item.checked = !item.checked
+      this.setData({ todoGroup })
+    },
+    addGroup() {
+      const { todoGroup } = this.data
+      todoGroup.push({
+        title: `new todo ${todoGroup.length + 1}`,
+        items: []
+      })
+      this.setData({ todoGroup })
+    }
   }
 })
